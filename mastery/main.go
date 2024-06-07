df1e5c7900157a25e1849789fc86d2d8926bb53f
thannoz <carlos-7@hotmail.de>
2024-06-07 22:11:24 +0200

    played around with interfaces

--- mastery/main.go
+++ mastery/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"mastery/interfaces"
+)
+
+func main() {
+	var inCal interfaces.Calculator
+	inCal = interfaces.SimpleCalculator{}
+	fmt.Println(inCal.Add(2, 3))
+
+}
