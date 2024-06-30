8ae570f41900e0d9d521a6faebbec7b1c017fd96
thannoz <carlos-7@hotmail.de>
2024-06-30 13:59:34 +0200

    fix: remove unnecessary comments

--- mastery/array_slices/array_slices_test.go
+++ mastery/array_slices/array_slices_test.go
@@ -5,15 +5,6 @@ import (
 	"testing"
 )
 
-/*
-1. Deklariere und initialisiere ein Array von 5 ganzen Zahlen und fülle es mit den Werten 1 bis 5.
-
-Array-Elemente ändern
-2. Ändere das zweite Element des Arrays in den Wert 10 und drucke das Array aus.
-
-Array-Länge
-3. Schreibe eine Funktion, die die Länge eines Arrays zurückgibt. */
-
 func TestArrayToIncludeFiveDigits(t *testing.T) {
 	t.Parallel()
 
